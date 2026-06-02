bf191a231f4742dc2b445675ecdb7bdc92d53dfe
Shawn Arnold <SArnoldEmbedSysEng@gmail.com>
2026-06-01 21:43:24 -0400

    Add Kellerman\'s Stripe link.

--- src/components/RegistrationSection.tsx
+++ src/components/RegistrationSection.tsx
@@ -11,7 +11,8 @@ export function RegistrationSection() {
             <p>Sterling Men&apos;s Weekend graduates only. Limited spots available.</p>
             <div className="registration-buttons">
               <a
-                href="https://app.tickettailor.com/events/spiritweekend20261/2222583"
+                // href="https://app.tickettailor.com/events/spiritweekend20261/2222583"
+                href="https://buy.stripe.com/28E14gdbTcM88Z4d643ks01"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="button button--primary pay-here-btn"
